e505945e835768e5eef2a89f4a40b4a7ac409b6b
Rees Draminski <rees.draminski@gmail.com>
2024-08-19 18:33:29 -0400

    update jekyll_utils.py

--- jekyll_utils.py
+++ jekyll_utils.py
@@ -16,7 +16,7 @@ from pydriller import Repository
 post_directory = "_posts"
 
 
-def get_changelog():
+def update_changelog():
     # changelog dict to group entries by date
     changelog = defaultdict(list)
 
@@ -51,7 +51,7 @@ def get_changelog():
         }, indent=4))
 
 
-def update_posts():
+def format_frontmatter():
     # inspired by: https://landscapearchaeology.org/2019/frontmatter/
     for file_name in os.listdir(post_directory):
         # get file path to post within post directory
@@ -63,33 +63,10 @@ def update_posts():
         # load post with python-frontmatter
         post = frontmatter.load(file_path)
 
-        # if post["type"] in ["recipe", "project"]:
-        #     post["layout"] = post["type"]
-
-        tags = post.get("tags")
-
-        if tags and "Mains" in tags:
-            tags[tags.index("Mains")] = "Main Courses"
-
-        # if post.get("category") == "Garden":
-        #     post["category"] = "Posts"
-
-        # if post.get("categories"):
-        #     category = post.get("categories")
-        #     del post["categories"]
-
-        #     post["category"] = category
-
-        # if post.get("categories") and post["categories"] in ["Articles", "Notes", "Stubs", "Lists"]:
-        #     # post["categories"] = "Garden"
-        #     del post["categories"]
-
-        #     post["category"] = "Garden"
-
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(frontmatter.dumps(post))
 
 if __name__ == "__main__":
-    # update_posts()
+    format_frontmatter()
 
-    get_changelog()
+    update_changelog()
