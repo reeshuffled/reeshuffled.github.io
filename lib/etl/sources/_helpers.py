d939dbfc0042a23682c3fe4f747432ffec6c8aa9
Rees <hi@reeswrites.com>
2026-06-04 11:37:59 -0400

    fix book review formatting

--- lib/etl/sources/_helpers.py
+++ lib/etl/sources/_helpers.py
@@ -5,4 +5,5 @@ import re
 
 
 def _strip_html(text: str) -> str:
-    return re.sub(r"<[^>]+>", "", html.unescape(text or "")).strip()
+    text = re.sub(r"<br\s*/?>", "\n", html.unescape(text or ""), flags=re.IGNORECASE)
+    return re.sub(r"<[^>]+>", "", text).strip()
